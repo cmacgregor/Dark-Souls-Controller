b90487efde93e595c2e9a19553bb8bc58c794990
BKING <macgregoco@gmail.com>
2020-04-11 19:32:22 -0500

    Added OverWorldCharacterClass
    Added OverWorldCharacter class
    Created Interfaces for the components required of an over world character

---
+++ Dark-Souls-Controller/Assets/Scripts/Overworld/OverworldCharacter.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverworldCharacter : MonoBehaviour {
+
+    public ICharacterController characterController;
+    public IAnimator animationTree;
+    public IActionController actionController;
+
+
+}
