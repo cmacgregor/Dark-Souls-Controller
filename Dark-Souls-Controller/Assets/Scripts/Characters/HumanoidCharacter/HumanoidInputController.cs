2c89971bf99eb092195ac243454a3f0fcf3a53f2
Connor MacGregor <macgregoco@gmail.com>
2020-05-15 13:33:16 -0500

    Moved input parsing into local input controller for humanoid character

--- Dark-Souls-Controller/Assets/Scripts/Characters/HumanoidCharacter/HumanoidInputController.cs
+++ Dark-Souls-Controller/Assets/Scripts/Characters/HumanoidCharacter/HumanoidInputController.cs
@@ -4,18 +4,19 @@ namespace Characters.HumanoidCharacter
 {
     public class HumanoidInputController : CharacterInputController, IHumanoidInputController
     {
-        float axisHorizontal;
-        float axisVertical;
-        bool leftLightAction;
-        bool leftHeavyAction;
-        bool rightLightAction;
-        bool rightHeavyAction;
-        bool toggleTwoHanded;
-        bool useItem;
-        bool dodge;
-        bool interact;
-        bool leftSideItemCycle;
-        bool rightSideItemCycle;
+        protected float axisHorizontal;
+        protected float axisVertical;
+        protected bool leftLightAction;
+        protected bool leftHeavyAction;
+        protected bool rightLightAction;
+        protected bool rightHeavyAction;
+        protected bool toggleTwoHanded;
+        protected bool useItem;
+        protected bool sprint;
+        protected bool dodge;
+        protected bool interact;
+        protected bool leftSideItemCycle;
+        protected bool rightSideItemCycle;
 
         public float AxisHorizontal { get => axisHorizontal; }
         public float AxisVertical { get => axisVertical; }
@@ -25,28 +26,15 @@ namespace Characters.HumanoidCharacter
         public bool RightHeavyAction { get => rightHeavyAction; }
         public bool ToggleTwoHanded { get => toggleTwoHanded; }
         public bool UseItem { get => useItem; }
+        public bool Sprint { get => sprint; }
         public bool Dodge { get => dodge; }
         public bool Interact { get => interact; }
         public bool LeftSideItemCycle { get => leftSideItemCycle; }
         public bool RightSideItemCycle { get => rightSideItemCycle; }
-
+        
         public override void ParseInputs()
         {
-            //analog sticks
-            axisHorizontal = Input.GetAxis("HorizontalMovement");
-            axisVertical = Input.GetAxis("VerticalMovement");
-            //shoulder buttons
-            leftLightAction = Input.GetButtonDown("LeftLightAction");
-            leftHeavyAction = Input.GetButtonDown("LeftHeavyAction");
-            rightLightAction = Input.GetButtonDown("RightLightAction");
-            rightHeavyAction = Input.GetButtonDown("RightHeavyAction");
-            //face buttons
-            toggleTwoHanded = Input.GetButtonDown("ToggleTwoHanded");
-            useItem = Input.GetButtonDown("UseItem");
-            dodge = Input.GetButtonDown("Dodge");
-            interact = Input.GetButtonDown("Interact");
-            leftSideItemCycle = Input.GetButtonDown("LeftSideItemCycle");
-            rightSideItemCycle = Input.GetButtonDown("RightSideItemCycle");
+
         }
     }
 }
--- Dark-Souls-Controller/Assets/Scripts/Characters/HumanoidCharacter/LocalHumanoidCharacter/LocalHumanoidInputController.cs
+++ Dark-Souls-Controller/Assets/Scripts/Characters/HumanoidCharacter/LocalHumanoidCharacter/LocalHumanoidInputController.cs
@@ -9,11 +9,11 @@ namespace Characters.HumanoidCharacter.LocalHumanoidInputController
 
         public float ANALOG_DEAD_ZONE = 0.1f;
 
-        private bool usableItemCycle;
-        private bool skillCycle;
-        private bool gestureMenu;
-        private bool mainMenu;
-        private bool toggleTargetCamera;
+        protected bool usableItemCycle;
+        protected private bool skillCycle;
+        protected private bool gestureMenu;
+        protected private bool mainMenu;
+        protected private bool toggleTargetCamera;
 
         public bool DownItemCycle { get => usableItemCycle; }
         public bool UpItemCycle { get => skillCycle; }
@@ -23,7 +23,22 @@ namespace Characters.HumanoidCharacter.LocalHumanoidInputController
 
         public override void ParseInputs()
         {
-            base.ParseInputs();
+            //analog sticks
+            axisHorizontal = Input.GetAxis("HorizontalMovement");
+            axisVertical = Input.GetAxis("VerticalMovement");
+            //shoulder buttons
+            leftLightAction = Input.GetButtonDown("LeftLightAction");
+            leftHeavyAction = Input.GetButtonDown("LeftHeavyAction");
+            rightLightAction = Input.GetButtonDown("RightLightAction");
+            rightHeavyAction = Input.GetButtonDown("RightHeavyAction");
+            //face buttons
+            toggleTwoHanded = Input.GetButtonDown("ToggleTwoHanded");
+            useItem = Input.GetButtonDown("UseItem");
+            dodge = Input.GetButtonDown("Dodge");
+            interact = Input.GetButtonDown("Interact");
+            leftSideItemCycle = Input.GetButtonDown("LeftSideItemCycle");
+            rightSideItemCycle = Input.GetButtonDown("RightSideItemCycle");
+
             //analog sticks
             toggleTargetCamera = Input.GetButtonDown("ToggleTargetCamera");
             //face buttons
